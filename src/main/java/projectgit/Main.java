6a9513d1cc573ad251d7a7ae0f2226a7b19e7612
mnafati <mehdi.nafati@yahoo.fr>
2018-06-01 16:27:14 +0200

    message

--- src/main/java/projectgit/Main.java
+++ src/main/java/projectgit/Main.java
@@ -1,8 +1,29 @@
 package projectgit;
 
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+
+@ComponentScan
+@SpringBootApplication
+@EnableAutoConfiguration
 public class Main {
+	
+	static int numA=10;
+	static int numB=-5;
+	static int numC=0;
 	
 	public static void main(String[] args) {	
 		System.out.println("Modification");
+
+		if (numA>numB) {
+			numC=numA+numB;
+		}
 	}
 }
--- src/test/java/projectgit/TestMain.java
+++ src/test/java/projectgit/TestMain.java
@@ -1,5 +1,23 @@
 package projectgit;
 
+
+
+import static org.junit.Assert.assertTrue;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class TestMain {
 
+	@Test
+	public void TestStupid(){
+		int numD=3;
+	assertTrue(numD==3);
+	}
+	
 }
